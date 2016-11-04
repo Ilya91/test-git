0f8e65477eb67367fa281d1d97419abcd8573163
Ilya <webmaster.1991@mail.ru>
2016-11-04 18:19:26 +0300

    ReCaptcha configuration

--- mail.php
+++ mail.php
@@ -13,32 +13,42 @@ if($v->validate()) {
     // Errors
     print_r($v->errors());
 }
-$output = 'Пользователь '.$_POST['name'].' оставил сообщение:<br/>';
-$output.= "<p>".$_POST['content'].'</p><br/>';
-$output.= "E-mail отправителя: ".$_POST['email'];
-$mail = new PHPMailer;
+$secret = '6LffGgsUAAAAAI5YlQ4a3r88xAsvzKmK1zF379A3';
+$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+if ($resp->isSuccess()) {
+    $output = 'Пользователь '.$_POST['name'].' оставил сообщение:<br/>';
+    $output.= "<p>".$_POST['text'].'</p><br/>';
+    $output.= "E-mail отправителя: ".$_POST['email'];
+    $mail = new PHPMailer;
 
-$mail->IsSMTP();
-$mail->SMTPAuth   = true;
-$mail->Host       = "smtp.yandex.ru";
-$mail->Username   = "mironchikov1991@yandex.ru";
-$mail->Password ='386met78';
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+    $mail->IsSMTP();
+    $mail->SMTPAuth   = true;
+    $mail->Host       = "smtp.yandex.ru";
+    $mail->Username   = "mironchikov1991@yandex.ru";
+    $mail->Password ='386met78';
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
 
-$mail->setFrom('mironchikov1991@yandex.ru', 'E-mail с сайта');
-$mail->addAddress('webmaster.1991@mail.ru', 'Получатель');     // Add a recipient
-$mail->addCC($_POST['email'], $_POST['name']);
-$mail->addReplyTo('mironchikov1991@yandex.ru', 'Robot');
-$mail->CharSet = 'UTF-8';
-$mail->isHTML(true);                                  // Set email format to HTML
-$mail->Subject = 'Письмо с сайта '.date('d.m.Y H:i:s',time());
-$mail->Body    = $output;
-$mail->AltBody = $output;
+    $mail->setFrom('mironchikov1991@yandex.ru', 'E-mail с сайта');
+    $mail->addAddress('webmaster.1991@mail.ru', 'Получатель');     // Add a recipient
+    $mail->addCC($_POST['email'], $_POST['name']);
+    $mail->addReplyTo('mironchikov1991@yandex.ru', 'Robot');
+    $mail->CharSet = 'UTF-8';
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Письмо с сайта '.date('d.m.Y H:i:s',time());
+    $mail->Body    = $output;
+    $mail->AltBody = $output;
 
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
+    if(!$mail->send()) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message has been sent';
+    }
 } else {
-    echo 'Message has been sent';
+    echo "Поражен вашей неудачей, сударь";
 }
+
+
+
